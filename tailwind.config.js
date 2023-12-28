419da7d0c4526c8f25704d2dd36dbd0b2f5afbbc
David de Almeida Cunha <daviid.0203@gmail.com>
2023-12-28 12:04:01 -0300

    refactor: config of default styles

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html, js, jsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      'red': '#D2202C',
+      'gray-dark': '#707070',
+      'gray': '#F5F5F7',
+      'gray-light': '#d3dce6',
+      'white': '#FFFFF',
+      'f-black': '#2F2E41',
+      'f-black-light': '#000000',
+      'black': '#323237',
+      'black-light': '#424146'
+    },
+  },
+  plugins: [],
+}
+
