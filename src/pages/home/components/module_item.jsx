d37b66b8be32cd74a448b78428b00bed0b996e89
David de Almeida Cunha <daviid.0203@gmail.com>
2024-01-02 16:19:43 -0300

    feat: Handling load module items

---
+++ src/pages/home/components/module_item.jsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react"
+import Peple_02 from '../../../assets/icons/people2.svg';
+import Clock from '../../../assets/icons/clock.svg';
+
+export const ModuleItem = ({course, key}) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=> {
+    const decodeImg = async () => {
+      try {
+        setLoading(true);
+        const image = new Image();
+        image.src = course.capa;
+        await image.decode();
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    decodeImg();
+  },[course, setLoading]);
+
+  return(loading ? <LoadModule key={key} /> : <Module key={key} course={course} />);
+}
+
+const Module = ({course, key}) => {
+  return(
+    <div key={key} className="flex items-center justify-between gap-5 bg-gray-light rounded-2xl p-5 mt-4">
+      <div className='flex h-full gap-4'>
+        <img className='h-32 w-32 rounded-md' src={course.capa} alt="capa" />
+
+        <div className='flex flex-col justify-around'>
+          <h4> {course.titulo} </h4>
+          <p className='text-sm font-semibold text-red'> {course.parceiros} </p>
+        </div>
+      </div>
+
+      <span className='flex items-center gap-2'>
+        <img className='w-6' src={Peple_02} alt="people" />
+        <p> {course.matriculados} (humanizar) </p>
+      </span>
+
+      <span className='flex items-center gap-2'>
+        <img className='w-6' src={Clock} alt="clock" />
+        <p> {course.duracao} </p>
+      </span>
+
+      <button className='bg-gray-dark rounded-full text-white text-lg font-semibold text-nowrap py-2 px-8'> Ver módulo </button>
+    </div>
+  )
+}
+
+const LoadModule = (key) => {
+  return(
+    <div key={key} className="flex animate-pulse items-center justify-between gap-5 bg-gray-light rounded-2xl p-5 mt-4">
+      <div className='flex h-full gap-4'>
+        <div className="flex justify-center items-center h-32 w-32 rounded-md bg-gray dark:bg-gray-dark">
+          <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+            <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z"/>
+            <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"/>
+          </svg>
+        </div>
+
+        <div className='flex flex-col justify-around'>
+          <div className="h-4 rounded w-96 bg-gray dark:bg-gray-dark" />
+          <div class="h-2 rounded w-72 bg-gray dark:bg-gray-dark" />
+        </div>
+
+      </div>
+
+      <div className="h-6 w-full rounded bg-gray dark:bg-gray-dark" />
+
+      <div className="h-6 w-full rounded bg-gray dark:bg-gray-dark" />
+
+      <div className="h-6 w-full rounded bg-gray dark:bg-gray-dark" />
+
+      <div className="h-12 w-full rounded bg-gray dark:bg-gray-dark" />
+  
+    </div>
+  );
+}
+
--- src/pages/home/sections/modules.jsx
+++ src/pages/home/sections/modules.jsx
@@ -1,19 +1,15 @@
-
-import Peple_02 from '../../../assets/icons/people2.svg';
-import Clock from '../../../assets/icons/clock.svg';
 import { useEffect, useState } from 'react';
 import { getThreeCoursers } from '../../../sevices/coursers';
 import { Link } from 'react-router-dom';
+import { ModuleItem } from '../components/module_item';
 
 
 export default function Modules() {
   const [filter, setFilter] = useState('matriculados');
   const [coursers, setCoursers] = useState([{},{},{}]);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getCoursers = async () => {
-      setLoading(true);
       const data = await getThreeCoursers(filter);
       setCoursers(data);
     }
@@ -45,30 +41,7 @@ export default function Modules() {
       </div>
 
       {coursers.map((course, index) => {
-        return(
-          <div className="flex items-center justify-between gap-5 bg-gray-light rounded-2xl p-5 mt-4">
-            <div className='flex h-full gap-4'>
-              <img className='h-32 w-32 rounded-md' src={course.capa} alt="capa" />
-
-              <div className='flex flex-col justify-around'>
-                <h4> {course.titulo} </h4>
-                <p className='text-sm font-semibold text-red'> {course.parceiros} </p>
-              </div>
-            </div>
-
-            <span className='flex items-center gap-2'>
-              <img className='w-6' src={Peple_02} alt="people" />
-              <p> {course.matriculados} (humanizar) </p>
-            </span>
-
-            <span className='flex items-center gap-2'>
-              <img className='w-6' src={Clock} alt="clock" />
-              <p> {course.duracao} </p>
-            </span>
-
-            <button className='bg-gray-dark rounded-full text-white text-lg font-semibold text-nowrap py-2 px-8'> Ver módulo </button>
-          </div>
-        );
+        return( <ModuleItem key={index} course={course} />);
       })}
 
       <button className='bg-gray-dark rounded-2xl text-white text-2xl w-96 font-semibold self-center py-2 mt-8'>
